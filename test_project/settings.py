79d68fbe423b5e6d7897b88bd05d48881de46139
manjunath <manjunath@agiliq.com>
2016-06-28 12:44:14 +0530

    first commit

--- test_project/settings.py
+++ test_project/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if 'heroku' in os.environ:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
